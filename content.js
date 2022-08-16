34ddafcdda09c230cb1934d1aa79e346530d7ef7
Chaitanya <guptachaitanya31@gmail.com>
2022-08-16 16:17:13 +0530

    start and stop audio event listeners added

--- content.js
+++ content.js
@@ -16,7 +16,7 @@ function startCheck() {
       console.log("micButtonExists");
       micButtonExists.remove();
     }
-    // if (!micButtonExists) {
+
     const micButton = document.createElement("img");
     micButton.className = "micButton";
     micButton.src = chrome.runtime.getURL("assets/microphone24_off.png");
@@ -24,10 +24,35 @@ function startCheck() {
     micButton.style.position = "relative";
     micButton.style.zIndex = "9999";
     micButton.style.right = "0px";
-    //   activeTextBox.value = "hello";
-    //   activeTextBox.style.backgroundColor = "red";
+    micButton.style.cursor = "pointer";
 
     activeTextBox.parentElement.appendChild(micButton);
-    // }
+    activeTextBox.parentElement.style.overflow = "visible";
+
+    micButton.addEventListener("click", startRecord);
   }
+}
+
+function startRecord() {
+  console.log("start clicked");
+  const micButton = document.getElementsByClassName("micButton")[0];
+  micButton.src = chrome.runtime.getURL("assets/microphone24_on.png");
+  micButton.title = "Stop";
+
+  // Code for starting recording and processing the audio
+
+  micButton.removeEventListener("click", startRecord);
+  micButton.addEventListener("click", stopRecord);
+}
+
+function stopRecord() {
+  console.log("stop clicked");
+  const micButton = document.getElementsByClassName("micButton")[0];
+  micButton.src = chrome.runtime.getURL("assets/microphone24_off.png");
+  micButton.title = "Speak";
+
+  // Code for stopping recording and processing the audio
+
+  micButton.removeEventListener("click", stopRecord);
+  micButton.addEventListener("click", startRecord);
 }
